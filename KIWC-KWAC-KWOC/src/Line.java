ae5f28c2b9516d94d2c65128c139221441f15a24
Jasontbk <jasontbk@gmail.com>
2013-08-24 20:29:31 +0800

    2nd commit!

--- KIWC-KWAC-KWOC/src/Line.java
+++ KIWC-KWAC-KWOC/src/Line.java
@@ -4,6 +4,11 @@ public class Line {
 	/**
 	 * @param args
 	 */
+	
+	String oString;
+	String[] tStrings;
+	int[][] indexes;
+	
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 
