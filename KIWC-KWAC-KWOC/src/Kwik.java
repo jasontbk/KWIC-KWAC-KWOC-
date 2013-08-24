2b2187857a1f52675e3dc0ffe957f59abca7767b
Jasontbk <jasontbk@gmail.com>
2013-08-24 22:15:37 +0800

    KWIK updated

--- KIWC-KWAC-KWOC/src/Kwik.java
+++ KIWC-KWAC-KWOC/src/Kwik.java
@@ -4,12 +4,22 @@ import java.util.Vector;
 
 public class Kwik {
 
-	private static Vector<Line> lines;
+	private static Vector<LineInterface> lines;
 	
 	public static void main(String[] args) {
 		
 		lines=ReadInput.readLine();
 
+		Line line;
+		for(int i=0;i<lines.size();i++){
+			line=(Line)lines.get(i);
+			System.out.println(Arrays.toString(line.getTokenizedString()));
+			for (int j=0;j<line.getSize();j++){
+				System.out.println(Arrays.toString(line.getIndexes()[j]));
+			}
+
+			
+		}
 
 	}
 
