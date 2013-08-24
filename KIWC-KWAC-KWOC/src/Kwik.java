db4f77539b13a356034aba7d66fab3b633396d20
Jasontbk <jasontbk@gmail.com>
2013-08-24 23:05:04 +0800

    More Kwik methods to readLines, shiftLines, sortStrings, printOutput

--- KIWC-KWAC-KWOC/src/Kwik.java
+++ KIWC-KWAC-KWOC/src/Kwik.java
@@ -1,26 +1,55 @@
-import java.util.Arrays;
+import java.util.Collections;
 import java.util.Vector;
 
-
 public class Kwik {
-	private static Vector<LineInterface> lines;
+	private Vector<LineInterface> lines;
+	private Vector<String> allStrings;
+	private String[] ignoreWords;
 
-	
 	public static void main(String[] args) {
-		
-		lines=ReadInput.readLine();
+		Kwik myKwik = new Kwik();
 
-		Line line;
-		for(int i=0;i<lines.size();i++){
-			line=(Line)lines.get(i);
-			System.out.println(Arrays.toString(line.getTokenizedString()));
-			for (int j=0;j<line.getSize();j++){
-				System.out.println(Arrays.toString(line.getIndexes()[j]));
-			}
+		myKwik.readLines();
+		myKwik.shiftLines();
+		myKwik.sortStrings();
+		myKwik.printOutput();
 
-			
+	}
+
+	private void readLines() {
+		lines = ReadInput.readLine();
+	}
+
+	private void readIgnoreWords() {
+		//Weizhi
+		//lines = ReadInput.readIgnoreWords();
+	}
+
+	private void shiftLines() {
+		// Weizhi
+
+	}
+
+	private void sortStrings() {
+
+		Collections.sort(allStrings, String.CASE_INSENSITIVE_ORDER);
+
+	}
+
+	private Boolean checkIgnored(String curr){
+		for(int j=0;j<ignoreWords.length;j++){
+			if (curr.startsWith(ignoreWords[j])) return false;
 		}
+		return true;
 
+	}
+
+	private void printOutput() {
+		for (int i = 0; i < allStrings.size(); i++) {
+			String curr = allStrings.get(i);
+			if (checkIgnored(curr))
+				System.out.println(curr);
+		}
 	}
 
 }
