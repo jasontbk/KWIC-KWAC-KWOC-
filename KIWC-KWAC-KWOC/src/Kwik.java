157ebe5c2112ec640a54fa8c61eb286fe376cc95
Zhi <Zhi@Zhi-PC>
2013-08-24 22:10:43 +0800

    -bug fix for Kwik.java

--- KIWC-KWAC-KWOC/src/Kwik.java
+++ KIWC-KWAC-KWOC/src/Kwik.java
@@ -4,11 +4,10 @@ import java.util.Vector;
 
 public class Kwik {
 
-	private static Vector<Line> lines;
 	
 	public static void main(String[] args) {
 		
-		lines=ReadInput.readLine();
+		Vector<LineInterface> lines=ReadInput.readLine();
 
 
 	}
